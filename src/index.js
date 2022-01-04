3a77c8e72d5e3a8715a2cae85b7b26762b9bab6a
Saull Brandão <saull@outlook.com>
2022-01-04 09:04:55 -0300

    feat: get customer statement

--- src/index.js
+++ src/index.js
@@ -28,4 +28,16 @@ app.post('/account', (req, res) => {
   return res.status(201).send()
 })
 
+app.get('/statement', (req, res) => {
+  const { cpf } = req.headers
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return res.status(400).json({ error: 'Customer not found' })
+  }
+
+  return res.json(customer.statement)
+})
+
 app.listen(3333)
