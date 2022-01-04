1564a4e630ae4b181defb76c09baa3ff065892fc
Saull Brandão <saull@outlook.com>
2022-01-04 09:27:07 -0300

    feat: customer can withdraw from account

--- src/index.js
+++ src/index.js
@@ -22,6 +22,16 @@ function verifyIfAccountExists(req, res, next) {
   return next()
 }
 
+function getAccountBalance(statement) {
+  return statement.reduce((acc, transaction) => {
+    if (transaction.type === 'credit') {
+      return acc + transaction.amount
+    }
+
+    return acc - transaction.amount
+  }, 0)
+}
+
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body
 
@@ -62,6 +72,27 @@ app.post('/deposit', verifyIfAccountExists, (req, res) => {
   }
 
   customer.statement.push(deposit)
+
+  return res.status(201).send()
+})
+
+app.post('/withdraw', verifyIfAccountExists, (req, res) => {
+  const { customer } = req
+
+  const { amount } = req.body
+  const balance = getAccountBalance(customer.statement)
+
+  if (amount > balance) {
+    return res.status(400).json({ error: 'Insufficient funds' })
+  }
+
+  const withdraw = {
+    amount,
+    created_at: new Date(),
+    type: 'debit',
+  }
+
+  customer.statement.push(withdraw)
 
   return res.status(201).send()
 })
