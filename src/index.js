ec723c929b5afaea13fbca021d796d26ad02ff4a
Saull Brandão <saull@outlook.com>
2022-01-04 09:36:12 -0300

    feat: get statement by date

--- src/index.js
+++ src/index.js
@@ -97,4 +97,19 @@ app.post('/withdraw', verifyIfAccountExists, (req, res) => {
   return res.status(201).send()
 })
 
+app.get('/statement/date', verifyIfAccountExists, (req, res) => {
+  const { customer } = req
+  const { date } = req.query
+
+  const dateFormat = new Date(date + ' 00:00')
+
+  const statement = customer.statement.filter(
+    transaction =>
+      transaction.created_at.toDateString() ===
+      new Date(dateFormat).toDateString(),
+  )
+
+  return res.json(statement)
+})
+
 app.listen(3333)
