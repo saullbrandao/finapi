cedb0120bfc0fd6f717232a91cd90cc41115f7d1
Saull Brandão <saull@outlook.com>
2022-01-04 09:14:41 -0300

    refactor: use a middleware to verify if account exists

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { request } = require('express')
 const express = require('express')
 const { v4: uuidv4 } = require('uuid')
 
@@ -6,6 +7,20 @@ const app = express()
 app.use(express.json())
 
 const customers = []
+
+function verifyIfAccountExists(req, res, next) {
+  const { cpf } = req.headers
+
+  const customer = customers.find(customer => customer.cpf === cpf)
+
+  if (!customer) {
+    return res.status(400).json({ error: 'Customer not found' })
+  }
+
+  request.customer = customer
+
+  return next()
+}
 
 app.post('/account', (req, res) => {
   const { cpf, name } = req.body
@@ -28,14 +43,8 @@ app.post('/account', (req, res) => {
   return res.status(201).send()
 })
 
-app.get('/statement', (req, res) => {
-  const { cpf } = req.headers
-
-  const customer = customers.find(customer => customer.cpf === cpf)
-
-  if (!customer) {
-    return res.status(400).json({ error: 'Customer not found' })
-  }
+app.get('/statement', verifyIfAccountExists, (req, res) => {
+  const { customer } = req
 
   return res.json(customer.statement)
 })
